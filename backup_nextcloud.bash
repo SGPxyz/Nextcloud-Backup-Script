6671b40c2fdc459eef0938c984e00b1a5b893c38
SGPxyz <96059610+SGPxyz@users.noreply.github.com>
2023-02-08 19:47:28 +0100

    Create backup_nextcloud.bash

---
+++ backup_nextcloud.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+###################################### NEXTCLOUD BACKUP SCRIPT ########################################
+#
+# NEXTCLOUD BACKUP SCRIPT is a simple bash script allowing to automatically backuping your Nextcloud instance
+# based on the guidelines of the official documentation
+#
+# Copyright (C) 2022 CypherXXX
+# Conceptually based on Nextcloud Official documentation:
+# https://docs.nextcloud.com/server/latest/admin_manual/maintenance/backup.html
+# 
+# If you like my work, use it or just want to support the devlopment of this script:
+# buymeacoffee.com/cypherxxxx https://paypal.me/cypherx
+# https://paypal.me/cypherx?country.x=FR&locale.x=fr_FR
+########################################################################################################
+
